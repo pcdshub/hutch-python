151caccd0488eaea0053771aedc6d22d31a3245f
Zachary Lentz <zlentz@slac.stanford.edu>
2018-11-02 20:19:03 -0700

    TST: fix invalid names in tests

--- hutch_python/tests/experiments/x010.py
+++ hutch_python/tests/experiments/x010.py
@@ -1,9 +1,9 @@
-from tst.db import calc_thing, tst_device_1, tst_device_2
+from tst.db import calc_thing, tst_device1, tst_device2
 from tst.beamline import unique_device
 
 
 class User:
     some_device = unique_device
     function = calc_thing
-    tst1 = tst_device_1
-    tst2 = tst_device_2
+    tst1 = tst_device1
+    tst2 = tst_device2
