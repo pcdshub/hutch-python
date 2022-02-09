8989b6863b78d71c08bec5a69d00c1e29835de34
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-02-09 22:36:17 +0000

    Update docs after building Travis build 616 of pcdshub/hutch-python
    The docs were built from the tag 'master' against the commit
    d42c3e81ec548b2f16011a5ce376ea5f9d4f0cf7.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/hutch-python/jobs/559058229.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.12/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.13.1',
+    VERSION: '1.13.1+2.gd42c3e8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
