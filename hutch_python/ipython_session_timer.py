10080e91257567aad755006bca060f371a9a052d
Jane Liu <janeliu@slac.stanford.edu>
2024-06-07 20:05:45 -0700

    Updated to allow user to set the number of hours before hutch-python session closes.

--- hutch_python/ipython_session_timer.py
+++ hutch_python/ipython_session_timer.py
@@ -6,6 +6,7 @@ maximum idle time can be updated. Currently, it is set to 48 hours.
 """
 
 import time
+import sys, select
 from IPython import get_ipython
 from threading import Thread
 
@@ -43,7 +44,7 @@ class IPythonSessionTimer:
 
     def __init__(self, ipython):
         self.curr_time = 0
-        self.max_idle_time = 172800
+        self.max_idle_time = 30  # 86400 is number of seconds in 24 hours
         self.last_active_time = 0
         self.idle_time = 0
 
@@ -63,12 +64,38 @@ class IPythonSessionTimer:
     def _start_session(self):
 
         # Check if idle_time has exceeded max_idle_time
-        while (self.idle_time < self.max_idle_time):
-            self._timer(self.max_idle_time - self.idle_time)
-            self._get_time_passed()
+        while (self.idle_time < self.max_idle_time):            # 0 < 50
+            self._timer(self.max_idle_time - self.idle_time)    # set timer to 50 sec
+            self._get_time_passed()                             # 50 sec have passed
 
-        # Close the user session
-        print("This hutch-python session has timed out. Please start a new session.")
+            if (self.idle_time >= self.max_idle_time):          # true
+                print("This hutch-python session has been idle for 1 day and will automatically close in 24 hours. \nAny code that is currently running will continue to run until it is completed.\n If you would like to keep this session active longer, enter the number of hours here (max is 96): "))
+                
+                # Set a timeout for user input
+                select.select([sys.stdin], [], [], 10)
+                extend_hours = int(sys.stdin.readline())
+                attempts = 0
+
+                # Any data read by sys.stdin is of type string even if it's cast as int. 
+                # But if the data is cast into int it, math operations can be perfomrmed on it.
+                # Checking if extend_hours is int will evaluate to false, so checking if 
+                # (extend_hours/2) is equal to an integer.
+
+                while ((extend_hours/2) is not int and attempts < 4):
+                    attempts += 1
+                    print("An incorrect input was detect. Please enter the number of hours to extend this session: ")
+                    select.select([sys.stdin], [], [], 10)
+                    extend_hours = int(sys.stdin.readline())
+
+                if (type(extend_hours/2) is int):
+                    if (extend_hours > 96):
+                        self.max_idle_time = 96  # (96 * 60 * 60)
+                        print("This session will be extended for 96 more hours.")
+                    else:
+                        self.max_idle_time = int(extend_hours)  # (extend_hours * 60 * 60)
+                        print("This session will be extended for %d more hours." % extend_hours)
+                else:
+                    print("The number of hours was not entered. This session will close. Any code that is currently\n running will continue to run until it is completed (to see results\n in the console please do not close this window).")
 
         # Close this ipython session
         get_ipython().ask_exit()
@@ -90,4 +117,3 @@ def load_ipython_extension(ipython):
     UserSessionTimer = IPythonSessionTimer(ipython)
     t1 = Thread(target=UserSessionTimer._start_session, daemon=True)
     t1.start()
-  
